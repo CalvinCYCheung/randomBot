ebba54ee009265fb588221b4d26846c53a932acd
calvin <61490calvin@gmail.com>
2022-10-23 01:48:11 +0800

    function logic update

--- app/lib/functions.dart
+++ app/lib/functions.dart
@@ -3,8 +3,10 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:app/data/command/tel_bot_command.dart';
+import 'package:app/data/controller/action_controller.dart';
 import 'package:app/data/controller/message_controller.dart';
 import 'package:app/data/model/income_data/tel_data.dart';
+import 'package:app/utils/callback_data_detector/data_detector.dart';
 export 'package:app/data/model/income_data/tel_data.dart';
 import 'package:app/utils/global.dart';
 import 'package:functions_framework/functions_framework.dart';
@@ -17,20 +19,38 @@ Future<void> function(TelData? data, RequestContext? context) async {
   logger.wtf(context?.request.headers);
   logger.d(data);
   if (data == null) return;
-  if (data.message == null) {
-    return;
+  if (data.callBackQuery != null) {
+    if (DataDetector()
+        .match(data.callBackQuery!.data!, DetectDataType.placeId)) {
+      final placeId = DataDetector().subString(data.callBackQuery!.data!);
+      final result = await ActionController()
+          .restaurantCallBackQuery(data.callBackQuery!.chatId, placeId);
+      if (result == null) return;
+      MessageController().searchCallBackResponse(data, result);
+    }
   }
-  if (data.message!.isCommand) {
-    switch (data.message!.command) {
-      case TelBotCommand.notCommand:
-      case TelBotCommand.general:
-        MessageController().generalResponse(data);
-        break;
+  if (data.message != null) {
+    if (data.message!.isCommand) {
+      switch (data.message!.command) {
+        case TelBotCommand.notCommand:
+        case TelBotCommand.general:
+          MessageController().generalResponse(data);
+          break;
+        case TelBotCommand.restaurant:
+          MessageController().shareLocation(data);
+          break;
+        case TelBotCommand.removekeyboard:
+          MessageController().removeReplyKeyboard(data);
+          break;
+      }
+    } else {
+      if (data.message!.location != null) {
+        final result = await ActionController().searchLocation(data);
+        if (result == null) {
+          return;
+        }
+        MessageController().searchResult(data, result);
+      }
     }
-  } else {
-    if (data.message!.location != null) {
-      MessageController().searchLocation(data);
-    }
-    MessageController().testReply(data);
   }
 }
