96570f0220414dc6c23b5cb23ae2263604efb3c3
calvin <61490calvin@gmail.com>
2022-11-04 04:29:56 +0800

    feat: add callback  data for new api , split method

--- app/lib/utils/callback_data_detector/data_detector.dart
+++ app/lib/utils/callback_data_detector/data_detector.dart
@@ -6,9 +6,23 @@ class DataDetector {
     return dataType.regExp.hasMatch(val);
   }
 
-  String subString(String val) {
+  String getSuffixString(String val) {
     final stringLength = val.substring(0, val.indexOf('/')).length + 1;
     return val.substring(stringLength);
+  }
+
+  String getPrefixString(String val) {
+    final stringLength = val.substring(val.length, val.indexOf('/')).length;
+    return val.substring(stringLength);
+  }
+
+  List<String> getSplitString(String val) {
+    return val.split('/');
+  }
+
+  bool isRestaurantsCallBack(String callbackData) {
+    return RestaurantsCallBackAction.values.data
+        .any((element) => callbackData.startsWith(element));
   }
 }
 
@@ -21,5 +35,33 @@ extension DetectDataTypeExtension on DetectDataType {
     return [
       RegExp(r"placeId/"),
     ][index];
+  }
+}
+
+enum RestaurantsCallBackAction {
+  restaurantData,
+  previousPage,
+  nextPage;
+
+  String offset(int offset) {
+    switch (this) {
+      case RestaurantsCallBackAction.restaurantData:
+        return '';
+      case RestaurantsCallBackAction.previousPage:
+        return '$name/offset:$offset';
+      case RestaurantsCallBackAction.nextPage:
+        return '$name/offset:$offset';
+    }
+  }
+
+  String get data {
+    return '$name/';
+  }
+}
+
+extension ListRestaurantsCallBackActionExtension
+    on List<RestaurantsCallBackAction> {
+  List<String> get data {
+    return map((e) => '${e.name}/').toList();
   }
 }
