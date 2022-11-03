fdda6bb2711628abe0949332bbffde6e01ab9350
calvin <61490calvin@gmail.com>
2022-11-04 04:30:16 +0800

    refactor: call new api data

--- app/lib/functions.dart
+++ app/lib/functions.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 
 import 'package:app/data/command/tel_bot_command.dart';
 import 'package:app/data/controller/action_controller.dart';
+import 'package:app/data/controller/callback_message_controller.dart';
 import 'package:app/data/controller/message_controller.dart';
 import 'package:app/data/model/income_data/tel_data.dart';
 import 'package:app/utils/callback_data_detector/data_detector.dart';
@@ -22,11 +23,14 @@ Future<void> function(TelData? data, RequestContext? context) async {
   if (data.callBackQuery != null) {
     if (DataDetector()
         .match(data.callBackQuery!.data!, DetectDataType.placeId)) {
-      final placeId = DataDetector().subString(data.callBackQuery!.data!);
+      final placeId = DataDetector().getSuffixString(data.callBackQuery!.data!);
       final result = await ActionController()
           .restaurantCallBackQuery(data.callBackQuery!.chatId, placeId);
       if (result == null) return;
       MessageController().searchCallBackResponse(data, result);
+    }
+    if (DataDetector().isRestaurantsCallBack(data.callBackQuery!.data!)) {
+      MessageController().restaurantsCallBackResponse(data);
     }
   }
   if (data.message != null) {
@@ -45,11 +49,14 @@ Future<void> function(TelData? data, RequestContext? context) async {
       }
     } else {
       if (data.message!.location != null) {
-        final result = await ActionController().searchLocation(data);
+        /// final result = await ActionController().searchLocation(data);
+        final result = await ActionController().getHKRestaurants(data, 0);
         if (result == null) {
           return;
         }
-        MessageController().searchResult(data, result);
+        MessageController().restaurantsResponse(data, result);
+
+        /// MessageController().searchResult(data, result);
       }
     }
   }
