bc815946ec9ef8de10000bc1a79a27f3424a758c
CalvinCYCheung <96610325+CalvinCYCheung@users.noreply.github.com>
2022-12-27 20:54:50 +0800

    delete sercet

--- app/lib/secret/secret.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dotenv/dotenv.dart';
-
-class EnvSecrets {
-  static const bot = "404720724:AAHzx0pmHXVD8BJSOgsgyagUkjQ8nGC59m8";
-  static const map = "AIzaSyC4oW3jHRqyyfSL5olSPj6AtyDtcIkOuV8";
-}
